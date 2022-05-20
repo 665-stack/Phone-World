22d3142e69dde87c89394646fdb3575fa813a1a6
unknown <mahmodulshahin665@gmail.com>
2022-05-20 23:46:09 +0600

    error handle

--- js/index.js
+++ js/index.js
@@ -2,24 +2,31 @@
 // get search keyword from input box
 const searchPhone = () => {
     const searchText = document.getElementById('search-input').value;
-    console.log(searchText);
+    loadPhone(searchText);
 
+    // clear search input box
     document.getElementById('search-input').value = '';
 }
 
 
-// ====== load Phone from API =======
+// ====== load Phone from api =======
 const loadPhone = searchText => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
 }
-loadPhone("samsung")
+
 // ====== display API's info ========
 const displayPhone = phones => {
     const container = document.getElementById('phones');
-    phones.forEach(phone => {
+    // error handle
+    const errorMassages = document.getElementById('errorMassages');
+    container.textContent = '';
+    if (phones) {
+        errorMassages.innerText = "Please enter phone model"
+    }
+    phones?.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add("perPhone");
@@ -38,6 +45,7 @@ const displayPhone = phones => {
        </div>
         
         `
+        errorMassages.innerText = "";
         container.appendChild(div);
     });
 }
